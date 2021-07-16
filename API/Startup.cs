8fdec91152ac5e6830554fa68a770802ca605fef
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-16 19:45:33 -0400

    Refactor config in startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -16,12 +16,11 @@ namespace API
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
