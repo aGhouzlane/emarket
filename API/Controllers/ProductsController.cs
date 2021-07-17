b39a37b0ca09878d68346fe2bf3ac87791e88d91
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-16 22:22:18 -0400

    Add logic to get the product form db

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,21 +1,31 @@
+using System.Linq;
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ProductsController : ControllerBase
+  {
+
+    private readonly StoreContext _context;
+    public ProductsController(StoreContext context)
     {
-        [HttpGet]
-        public string GetProducts()
-        {
-            return "products list";
-        }
-        
-        [HttpGet("{id}")]
-        public string GetProduct(int id)
-        {
-            return "single product";
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    public ActionResult GetProducts()
+    {
+      var products = _context.Products.ToList();
+      return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public string GetProduct(int id)
+    {
+      return "single product";
+    }
+  }
 }
