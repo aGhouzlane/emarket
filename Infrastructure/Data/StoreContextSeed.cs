539135f7f24290372b456ef7f6990faca1083fc0
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-23 16:01:36 -0400

    Add logic to read brands.json and save it to db

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -7,7 +12,21 @@ namespace Infrastructure.Data
     {
         public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
         {
+            try
+            {
+                if(!context.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
 
+                    foreach(var item in brands)
+                    {
+                        context.ProductBrands.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
         }
     }
 }
