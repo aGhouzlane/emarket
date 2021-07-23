ff6498e87fea8c8cade46b8ea36309ac5d00c6fb
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-23 16:11:41 -0400

    Add logic to read producttypes and products and save in db

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -8,25 +8,53 @@ using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
 {
-    public class StoreContextSeed
+  public class StoreContextSeed
+  {
+    public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
     {
-        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+      try
+      {
+        if (!context.ProductBrands.Any())
         {
-            try
-            {
-                if(!context.ProductBrands.Any())
-                {
-                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
-                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+          var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+          var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
 
-                    foreach(var item in brands)
-                    {
-                        context.ProductBrands.Add(item);
-                    }
+          foreach (var item in brands)
+          {
+            context.ProductBrands.Add(item);
+          }
 
-                    await context.SaveChangesAsync();
-                }
-            }
+          await context.SaveChangesAsync();
         }
+
+        if (!context.ProductTypes.Any())
+        {
+          var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+          var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+          foreach (var item in types)
+          {
+            context.ProductTypes.Add(item);
+          }
+
+          await context.SaveChangesAsync();
+        }
+
+        if (!context.Products.Any())
+        {
+          var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+          var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+          foreach (var item in products)
+          {
+            context.Products.Add(item);
+          }
+
+          await context.SaveChangesAsync();
+        }
+
+      }
+
     }
+  }
 }
