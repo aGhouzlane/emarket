0527abb7ffc1cd2a85497c5b567ae635d3ff39f5
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-26 18:16:56 -0400

    Update getproducts and getproductbyid to include brand and type

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -11,7 +11,7 @@ namespace Infrastructure.Data
     private readonly StoreContext _context;
     public ProductRepository(StoreContext context)
     {
-        _context = context;
+      _context = context;
     }
 
     public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
@@ -21,12 +21,18 @@ namespace Infrastructure.Data
 
     public async Task<Product> GetProductByIdAsync(int id)
     {
-      return await _context.Products.FindAsync(id);
+      return await _context.Products
+            .Include(p => p.ProductBrand)
+            .Include(p => p.ProductType)
+            .FirstOrDefaultAsync(p => p.Id == id);
     }
 
     public async Task<IReadOnlyList<Product>> GetProductsAsync()
     {
-      return await _context.Products.ToListAsync();
+      return await _context.Products
+            .Include(p => p.ProductBrand)
+            .Include(p => p.ProductType)
+            .ToListAsync();
     }
 
     public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
