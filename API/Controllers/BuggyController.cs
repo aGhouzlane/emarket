2cce042ffa53520d3cb17b44782d98819e4c8e3d
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-27 17:59:00 -0400

    Add buggycontroller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,50 @@
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  public class BuggyController : BaseApiController
+  {
+    private readonly StoreContext _context;
+    public BuggyController(StoreContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet("notfound")]
+    public ActionResult GetNotFoundRequest()
+    {
+      var thing = _context.Products.Find(23);
+
+      if (thing == null)
+      {
+        return NotFound();
+      }
+
+      return Ok();
+    }
+
+    [HttpGet("servererror")]
+    public ActionResult GetServerError()
+    {
+      var thing = _context.Products.Find(23);
+
+      var thingToReturn = thing.ToString();
+
+      return Ok();
+    }
+
+    [HttpGet("notfound")]
+    public ActionResult GetBadRequest()
+    {
+
+      return BadRequest();
+    }
+
+    [HttpGet("notfound")]
+    public ActionResult GetNotFoundRequest()
+    {
+      return Ok();
+    }
+  }
+}
