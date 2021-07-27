0188e21835cf82e09d68d325f32e4d3a65148651
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-27 13:17:50 -0400

    Add class to change shape of data returned from api

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -38,7 +38,7 @@ namespace Infrastructure.Data
 
     private IQueryable<T> ApplySpecification(ISpecification<T> spec)
     {
-        return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+      return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
     }
   }
 }
