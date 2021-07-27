448889775232180c4caf8d9c405d8236c89f8333
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-27 14:53:51 -0400

    Add logic to handle mapping brands and types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using System.Linq;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -16,14 +17,17 @@ namespace API.Controllers
     private readonly IGenericRepository<Product> _productsRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
     private readonly IGenericRepository<ProductType> _productTypeRepo;
+    private readonly IMapper _mapper;
     public ProductsController(IGenericRepository<Product> productsRepo,
     IGenericRepository<ProductBrand> productBrandRepo,
-    IGenericRepository<ProductType> productTypeRepo
+    IGenericRepository<ProductType> productTypeRepo,
+    IMapper mapper
     )
     {
       _productsRepo = productsRepo;
       _productBrandRepo = productBrandRepo;
       _productTypeRepo = productTypeRepo;
+      _mapper = mapper;
     }
 
     [HttpGet]
@@ -48,16 +52,7 @@ namespace API.Controllers
     {
       var spec = new ProductTypeAndBrandSpecification(id);
       var product = await _productsRepo.GetEntityWithSpec(spec);
-      return new ProductToReturnDto
-      {
-        Id = product.Id,
-        Name = product.Name,
-        Description = product.Description,
-        PictureUrl = product.PictureUrl,
-        Price = product.Price,
-        ProductBrand = product.ProductBrand.Name,
-        ProductType = product.ProductType.Name
-      };
+      return _mapper.Map<Product, ProductToReturnDto>(product);
     }
 
     [HttpGet("brands")]
