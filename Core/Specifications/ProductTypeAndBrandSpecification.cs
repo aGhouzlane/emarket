6684755b1f9da366152143f34135bb4f39ec4970
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-28 13:07:41 -0400

    Add logic to sort by price

--- Core/Specifications/ProductTypeAndBrandSpecification.cs
+++ Core/Specifications/ProductTypeAndBrandSpecification.cs
@@ -6,16 +6,32 @@ namespace Core.Specifications
   {
     public ProductTypeAndBrandSpecification(string sort)
     {
-        AddInclude(x => x.ProductBrand);
-        AddInclude(x => x.ProductType);
-        AddOrderBy(x => x.Name);
+      AddInclude(x => x.ProductBrand);
+      AddInclude(x => x.ProductType);
+      AddOrderBy(x => x.Name);
+
+      if (!string.IsNullOrEmpty(sort))
+      {
+        switch (sort)
+        {
+          case "priceAsc":
+            AddOrderBy(p => p.Price);
+            break;
+          case "priceDesc":
+            AddOrderByDescending(p => p.Price);
+            break;
+          default:
+            AddOrderBy(n => n.Name);
+            break;
+        }
+      }
     }
 
     public ProductTypeAndBrandSpecification(int id)
     : base(x => x.Id == id)
     {
-        AddInclude(x => x.ProductBrand);
-        AddInclude(x => x.ProductType);
+      AddInclude(x => x.ProductBrand);
+      AddInclude(x => x.ProductType);
     }
   }
 }
