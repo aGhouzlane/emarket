5dbb2eb1e157d48f7c349b80367642e6bd613d12
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-29 13:54:57 -0400

    Add core module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
  
   title = 'EMarket';
+  products: any[];
 
   constructor(private http: HttpClient){
 
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
-      console.log(response);
+      this.products = response;
     });
   }
 }
